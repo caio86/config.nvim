29f4870f410733183360d03b98de0fca9fbf226e
caio86 <80562020+caio86@users.noreply.github.com>
2023-09-16 22:19:22 -0300

    feat: Updated harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,3 +1,36 @@
-if true then
-  return {}
-end
+return {
+  {
+    "ThePrimeagen/harpoon",
+    config = function(_, opts)
+      require("harpoon").setup(opts)
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon Add File" })
+
+      vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "Harpoon Toggle Quick Menu" })
+
+      vim.keymap.set("n", "<leader>1", function()
+        ui.nav_file(1)
+      end, { desc = "Harpoon File 1" })
+
+      vim.keymap.set("n", "<leader>2", function()
+        ui.nav_file(2)
+      end, { desc = "Harpoon File 2" })
+
+      vim.keymap.set("n", "<leader>3", function()
+        ui.nav_file(3)
+      end, { desc = "Harpoon File 3" })
+
+      vim.keymap.set("n", "<leader>4", function()
+        ui.nav_file(4)
+      end, { desc = "Harpoon File 4" })
+    end,
+  },
+  {
+    "ThePrimeagen/vim-be-good",
+    keys = {
+      { "<leader>vbg", "<cmd>VimBeGood<cr>", { desc = "Vim Be Good" } },
+    },
+  },
+}
