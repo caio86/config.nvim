418fc9ad2e9ddc02da9130eaa2794c9ab1ed7359
caio86 <80562020+caio86@users.noreply.github.com>
2023-09-16 22:21:23 -0300

    Added No Neck Pain plugin

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,26 +1,35 @@
 return {
-  "folke/zen-mode.nvim",
-  cmd = "ZenMode",
-  config = function()
-    require("zen-mode").setup({
-      window = {
-        backdrop = 0.95,
-        width = 120,
-        height = 1,
-        options = {
-          signcolumn = "no",
-          number = false,
-          relativenumber = false,
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    config = function()
+      require("zen-mode").setup({
+        window = {
+          backdrop = 0.95,
+          width = 120,
+          height = 1,
+          options = {
+            signcolumn = "no",
+            number = false,
+            relativenumber = false,
+          },
         },
-      },
-      plugins = {
-        gitsigns = { enabled = false },
-        tmux = { enabled = false },
-        twilight = { enabled = false },
-      },
-    })
-  end,
-  keys = {
-    { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+        plugins = {
+          gitsigns = { enabled = false },
+          tmux = { enabled = false },
+          twilight = { enabled = false },
+        },
+      })
+    end,
+    keys = {
+      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+    },
+  },
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    keys = {
+      { "<leader>Z", "<cmd>NoNeckPain<cr>", desc = "No Neck Pain" },
+    },
   },
 }
